b87db357b2cc4ed6bea404c4ffd75446bfb74a95
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2021-07-14 18:31:15 +0200

    init script for creating summary tables

---
+++ fb_ads_summary_tables.R
@@ -0,0 +1,131 @@
+library(dplyr)
+library(readr)
+library(tidyr)
+
+full_ads_table <- readRDS("data/merged_data.rds")
+
+if (!dir.exists("data/summary_tables")) {
+  dir.create("data/summary_tables")
+} else {
+  print("output directory already exists")  
+}
+
+# Creating a summary table focused on the detailed aspects of the advertising
+ad_summary <- full_ads_table %>% 
+              select(1:19) %>% 
+              group_by(page_name, page_id) %>% 
+              summarise(total_ads = n(),
+                        unique_ads = n_distinct(ad_creative_body),
+                        percent_unique = unique_ads / total_ads,
+                     # lower_spend = sum(spend_lower, na.rm = TRUE),
+                     # upper_spend = sum(spend_upper, na.rm = TRUE),
+                       avg_spend = (sum(spend_lower, na.rm = TRUE) + sum(spend_upper, na.rm = TRUE)) / 2,
+                     # total_lower_impressions = sum(impressions_lower, na.rm = TRUE),
+                     # total_upper_impressions = sum(impressions_upper, na.rm = TRUE),
+                       total_avg_impressions = (sum(impressions_lower, na.rm = TRUE) + sum(impressions_upper, na.rm = TRUE)) / 2,
+                       per_ad_avg_impression = total_avg_impressions / total_ads,
+                       total_min_reach = sum(potential_reach_lower, na.rm = TRUE),
+                       per_ad_min_reach = total_min_reach / total_ads,
+                       avg_ad_runtime = mean(ad_delivery_stop_time - ad_delivery_start_time, na.rm = TRUE)) %>% 
+              arrange(desc(total_ads))
+
+# Writing the table to a csv file
+write_csv(ad_summary, "data/summary_tables/ad_summary.csv")
+
+# Creating a summary table focused on the demographic aspects
+demographic_summary <- full_ads_table %>% 
+  transmute(page_name, 
+         page_id,
+         female_13_17 = `female_13-17`,
+         female_18_24 = `female_18-24`, 
+         female_25_34 = `female_25-34`,
+         female_35_44 = `female_35-44`,
+         female_45_54 = `female_45-54`,  
+         female_55_64 = `female_55-64`,   
+         female_65_plus = `female_65+`,     
+         male_13_17 = `male_13-17`,     
+         male_18_24 = `male_18-24`,  
+         male_25_34 = `male_25-34`,    
+         male_35_44 = `male_35-44`,    
+         male_45_54 = `male_45-54`,    
+         male_55_64 = `male_55-64`,     
+         male_65_plus = `male_65+`) %>% 
+  replace(is.na(.), 0) %>% 
+  group_by(page_name, page_id) %>% 
+  summarise(total_ads = n(),
+            avg_female_13_17 = mean(female_13_17),
+            avg_female_18_24 = mean(female_18_24),
+            avg_female_25_34 = mean(female_25_34),
+            avg_female_35_44 = mean(female_35_44),
+            avg_female_45_54 = mean(female_45_54),
+            avg_female_55_64 = mean(female_55_64),
+            avg_female_65_plus = mean(female_65_plus),
+            avg_male_13_17 = mean(male_13_17),
+            avg_male_18_24 = mean(male_18_24),
+            avg_male_25_34 = mean(male_25_34),
+            avg_male_35_44 = mean(male_35_44),
+            avg_male_45_54 = mean(male_45_54),
+            avg_male_55_64 = mean(male_55_64),
+            avg_male_65_plus = mean(male_65_plus),
+            avg_female = (avg_female_13_17 + avg_female_18_24 + avg_female_25_34 + avg_female_35_44 + avg_female_45_54 + avg_female_55_64 + avg_female_65_plus),
+            avg_male = (avg_male_13_17 + avg_male_18_24 + avg_male_25_34 + avg_male_35_44 + avg_male_45_54 + avg_male_55_64 + avg_male_65_plus),
+            avg_13_17 = (avg_female_13_17 + avg_male_13_17),
+            avg_18_24 = (avg_female_18_24 + avg_male_18_24),
+            avg_25_34 = (avg_female_25_34 + avg_male_25_34),
+            avg_35_44 = (avg_female_35_44 + avg_male_35_44),
+            avg_45_54 = (avg_female_45_54 + avg_male_45_54),
+            avg_55_64 = (avg_female_55_64 + avg_male_55_64),
+            avg_65_plus = (avg_female_65_plus + avg_male_65_plus)) %>% 
+  arrange(desc(total_ads))
+
+# Writing the table to a csv file
+write_csv(demographic_summary, "data/summary_tables/demographic_summary.csv")
+
+
+# Creating a summary table focused on the regional aspects
+# We rename the Czech regions using Czech Statistical Office abbreviations
+region_summary <- full_ads_table %>% 
+  transmute(page_name, 
+            page_id,
+            pha = `Prague`,
+            stc = `Central Bohemian Region`,
+            jhc = `South Bohemian Region`,
+            plk = `Plzeň Region`,
+            kvk = `Karlovy Vary Region`,
+            ulk = `Ústí nad Labem Region`,
+            lbk = `Liberec Region`,
+            hkk = `Hradec Králové Region`,
+            pak = `Pardubice Region`,
+            vys = `Vysočina Region`,
+            jhm = `South Moravian Region`,
+            olk = `Olomouc Region`,
+            msk = `Moravian-Silesian Region`,
+            zlk = `Zlín Region`) %>% 
+  replace(is.na(.), 0) %>% 
+  group_by(page_name, page_id) %>%
+  summarise(total_ads = n(),
+            avg_pha = mean(pha),
+            avg_stc = mean(stc),
+            avg_jhc = mean(jhc),
+            avg_plk = mean(plk),
+            avg_kvk = mean(kvk),
+            avg_ulk = mean(ulk),
+            avg_lbk = mean(lbk),
+            avg_hkk = mean(hkk),
+            avg_pak = mean(pak),
+            avg_vys = mean(vys),
+            avg_jhm = mean(jhm),
+            avg_olk = mean(olk),
+            avg_msk = mean(msk),
+            avg_zlk = mean(zlk)) %>% 
+  arrange(desc(total_ads))
+
+# Writing the table to a csv file
+write_csv(region_summary, "data/summary_tables/region_summary.csv")
+
+merged_summary <- ad_summary %>% 
+                  inner_join(demographic_summary, by = c("page_name", "page_id", "total_ads")) %>% 
+                  inner_join(region_summary, by = c("page_name", "page_id", "total_ads"))
+
+write_csv(merged_summary, "data/summary_tables/merged_summary.csv")
+saveRDS(object = merged_summary, file = "data/summary_tables/merged_summary.rds", compress = FALSE) 
--- fb_ads_summary_visuals.R
+++
@@ -1,5 +0,0 @@
-merged_table %>% 
-  group_by(page_name) %>% 
-  summarise(ad_spend = sum(spend_upper), praha_percentage = mean(Prague, na.rm = TRUE)) %>% 
-  arrange(desc(ad_spend))
-
